ec5fa2db3d3d937e683fbe4f38d01da3d7616fd8
Seth Weidman <sethweidman@Seths-MacBook-Pro.local>
2024-03-12 13:46:16 -0700

    Add initial layout

--- src/App.js
+++ src/App.js
@@ -3,10 +3,23 @@ import React from "react";
 const App = () => {
   return (
     <React.Fragment>
-      <h1 className="main-title">Local GPT</h1>
-      <p>
-        A way to interact with large language models locally on your laptop.
-      </p>
+      <div className="header-material">
+        <h1 className="main-title">Local GPT</h1>
+        <p>
+          A way to interact with large language models locally on your laptop.
+        </p>
+      </div>
+      <div className="app-container">
+        <div className="past-chats-panel">This area will display a list of your prior chats.</div>
+        <div className="current-llm-interaction">
+          <div className="llm-response-display">This will display the output from the LLM you are interacting with.</div>
+          <textarea
+            className="user-input"
+            placeholder="This is where you will type your query to the LLM"
+          ></textarea>          
+        </div>
+        <div className="control-bar">This area will contain controls, such as the "temperature", for the LLM you are interacting with.</div>
+      </div>
     </React.Fragment>
   );
 };
