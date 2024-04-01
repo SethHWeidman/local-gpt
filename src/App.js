6bdacc046266d4cbcced34fe036e062063166064
Seth Weidman <sethweidman@seths-mbp.lan>
2024-03-31 20:09:43 -0700

    Add loading modal

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { useState } from "react";
 import ControlPanel from './components/ControlPanel';
 import LLMResponse from './components/LLMResponse';
+import Modal from './components/Modal';
 
 const App = () => {
   const [userText, setUserText] = useState("");
   const [systemMessage, setSystemMessage] = useState(""); // New state for text from ControlPanel
-  const [llmResponse, setLlmResponse] = useState(""); // State to hold the LLM response  
+  const [llmResponse, setLlmResponse] = useState(""); // State to hold the LLM response
+  const [isModalVisible, setIsModalVisible] = useState(false);  
 
   // Callback function to update text from ControlPanel
   const handleSystemMessageChange = (event) => {
@@ -13,6 +15,8 @@ const App = () => {
   }
 
   const handleSubmit = async () => {
+    setIsModalVisible(true); // Show the modal
+
     console.log(systemMessage)
     try {
       const response = await fetch("http://localhost:5005/submit-interaction", {
@@ -26,15 +30,18 @@ const App = () => {
       console.log(data); // Log the response from the backend
       // Handle any post-submission logic here, like clearing the text area
       setLlmResponse(data['GPT-4 Response'])
-      setUserText(""); // Clear the text area after successful submission
-      setSystemMessage(""); // Clear the text area after successful submission
     } catch (error) {
       console.error("Error submitting text:", error);
+    } finally {
+      setIsModalVisible(false); // Hide the modal regardless of the request's outcome
+      // setUserText(""); // Clear the text area after successful submission
+      // setSystemMessage(""); // Clear the text area after successful submission
     }
   };
 
   return (
     <React.Fragment>
+      <Modal isVisible={isModalVisible} message="Retrieving response from LLM, please wait..." />
       <div className="header-material">
         <h1 className="main-title">Local GPT</h1>
         <p>
