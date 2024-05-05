873940b0e9b025c59b66492a15c2d3476ee89371
Seth Weidman <sethweidman@seths-mbp.lan>
2024-05-05 08:41:07 -0700

    Format App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React, { useState } from "react";
-import ControlPanel from './components/ControlPanel';
-import LLMResponse from './components/LLMResponse';
-import Modal from './components/Modal';
+import ControlPanel from "./components/ControlPanel";
+import LLMResponse from "./components/LLMResponse";
+import Modal from "./components/Modal";
 
 const App = () => {
   const [userText, setUserText] = useState("");
   const [systemMessage, setSystemMessage] = useState(""); // New state for text from ControlPanel
   const [llmResponse, setLlmResponse] = useState(""); // State to hold the LLM response
-  const [isModalVisible, setIsModalVisible] = useState(false);  
+  const [isModalVisible, setIsModalVisible] = useState(false);
 
   // Callback function to update text from ControlPanel
   const handleSystemMessageChange = (event) => {
-    setSystemMessage(event.target.value)
-  }
+    setSystemMessage(event.target.value);
+  };
 
   const handleSubmit = async () => {
     setIsModalVisible(true); // Show the modal
@@ -26,7 +26,7 @@ const App = () => {
         body: JSON.stringify({ userText, systemMessage }),
       });
       const data = await response.json();
-      setLlmResponse(data['GPT-4 Response'])
+      setLlmResponse(data["GPT-4 Response"]);
     } catch (error) {
       console.error("Error submitting text:", error);
     } finally {
@@ -36,7 +36,10 @@ const App = () => {
 
   return (
     <React.Fragment>
-      <Modal isVisible={isModalVisible} message="Retrieving response from LLM, please wait..." />
+      <Modal
+        isVisible={isModalVisible}
+        message="Retrieving response from LLM, please wait..."
+      />
       <div className="header-material">
         <h1 className="main-title">Local GPT</h1>
         <p>
@@ -55,9 +58,14 @@ const App = () => {
             onChange={(e) => setUserText(e.target.value)}
             placeholder="This is where you will type your query to the LLM"
           ></textarea>
-          <button className="submit-button" onClick={handleSubmit}>Submit</button>
+          <button className="submit-button" onClick={handleSubmit}>
+            Submit
+          </button>
         </div>
-        <ControlPanel onSystemMessageChange={handleSystemMessageChange} systemMessage={systemMessage}/>
+        <ControlPanel
+          onSystemMessageChange={handleSystemMessageChange}
+          systemMessage={systemMessage}
+        />
       </div>
     </React.Fragment>
   );
